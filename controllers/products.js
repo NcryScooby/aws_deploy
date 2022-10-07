ad78a23933ba1dd77bcd8f66ee35defa57e91a23
Fabrício <fabricio_c_anelli@hotmail.com>
2022-10-07 18:21:01 -0300

    #4 - Create product

--- controllers/products.js
+++ controllers/products.js
@@ -16,4 +16,21 @@ const getProducts = (req, res) => {
   });
 };
 
-module.exports = { getProducts };
+const createProduct = (req, res) => {
+  const { name, price } = req.body;
+  const SQL = `INSERT INTO products (name, price) VALUES ('${name}', ${price})`;
+
+  mysql.query(SQL, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        error: err,
+      });
+    } else {
+      res.status(200).json({
+        message: "Product created successfully",
+      });
+    }
+  });
+};
+
+module.exports = { getProducts, createProduct };
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 
 const authorization = require("../utils/authorization");
 
-const { getProducts } = require("../controllers/products");
+const { getProducts, createProduct } = require("../controllers/products");
 
 router.get("/getProducts", authorization, getProducts);
+router.post("/createProduct", authorization, createProduct);
 
 module.exports = router;
